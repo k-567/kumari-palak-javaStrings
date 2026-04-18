ebaf2e469f330204c0810e75fbdb4b4b743cb8dc
k-567 <kumari.bcr25204@imsnoida.com>
2026-04-18 18:46:07 +0530

    Create StringPractice.java

---
+++ StringPractice.java
@@ -0,0 +1,13 @@
+public class StringPractice {
+      public static void main(String[] args) {
+
+         String str1 = "Hello";
+         String str2 = "World";
+
+         System.out.println("Length: " + strl.length());
+         System.out.println("Concat: " + str1 + " " + str2);
+         System.out.println("Upper: " + str1.toUpperCase());
+         System.out.println("Substring: " + str1.substring(0, 3));
+         System.out.println("Equals: " + str1.equals(str2));
+      }
+}
